5e9d331eb33bd8fbf3e9a83e201cad3b266ccdf4
Herman Liem <hermanliemz@gmail.com>
2026-03-26 13:28:22 +0700

    Make email optional in create user form

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "C:\Users\herma\My Drive\Claude\Finance\Stock Opname Hotel\hotel-inventory"
 git add -A
-git commit -m "Fix department dropdown, change password page, and responsive mobile layout for all pages"
+git commit -m "Make email optional in create user form"
 git push
 pause
