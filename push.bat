b866a00e3f76887f21f16b64f71457778b5b7422
Herman Liem <hermanliemz@gmail.com>
2026-03-26 13:38:11 +0700

    Block transfer to In-Use Warehouse if same item still has stock, make email optional

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "C:\Users\herma\My Drive\Claude\Finance\Stock Opname Hotel\hotel-inventory"
 git add -A
-git commit -m "Make email optional in create user form"
+git commit -m "Block transfer to In-Use Warehouse if same item still has stock, make email optional"
 git push
 pause
